23dcdb3cebc8f1e80506fff926a7d13e5ca2141d
fvalean <36827144+fvalean@users.noreply.github.com>
2018-06-06 23:05:22 -0400

    eleted commented code

--- server.js
+++ server.js
@@ -25,11 +25,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-/*app.get('/', (req, res) => {
-	db.select('*').from('users').then(data => {
-		res.json(data);
-	});
-});*/
 app.get('/', (req, res) => {res.send('it is working!')});
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
