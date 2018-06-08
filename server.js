bdfcc8e79c62cde0e3211dc3dfac945cb6968b07
fvalean <36827144+fvalean@users.noreply.github.com>
2018-06-07 22:19:12 -0400

    added heroku specific database connection

--- server.js
+++ server.js
@@ -10,28 +10,36 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
+const db = process.env.DATABASE_URL ? knex({
 	client: 'pg',
 	connection: {
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
-		database: process.env.DB_NAME
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
-});
+}) :
+	knex({
+		client: 'pg',
+		connection: {
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			database: process.env.DB_NAME
+		}
+	})
+	;
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('it is working!')});
+app.get('/', (req, res) => { res.send('it is working!') });
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
-app.put('/image', (req, res) => {image.handleImage(req, res, db)});
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 
 app.listen(process.env.PORT, () => {
